2b5e0d0a7e06a20e6b38b43b8099fef5bc12ccad
BB <seven.albany.bi@gmail.com>
2020-09-05 23:41:24 +0800

    install jest and material-ui

---
+++ src/components/Grids.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+
+
+
+
+export default function Grids() {
+	return (
+		<div>
+			RGB Grids
+		</div>
+	);
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Grids from ".components/Grids.js";
+
+
+
+let ReadMe = () => {
+	return(
+		<div>
+			<span><h3>READ ME</h3></span>
+		</div>
+	)
+}
+
+class GridView extends React.Component {
+
+	render() {
+		return(
+			<div style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', height: '80vh'}}>
+				<Grids />
+				<ReadMe />
+			</div>
+		)
+	}
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<GridView />,
+	document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
